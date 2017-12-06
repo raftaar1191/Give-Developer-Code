6b1b5a5b7474120a7d8aa5ba6cbf918aaf5803e9
raftaar1191 <raftaar1191@gmail.com>
2017-12-07 04:59:45 +0530

    Plugin index file and plugin name added

---
+++ index.php
@@ -0,0 +1,9 @@
+/**
+* Plugin Name: Give - Development Plugin
+* Plugin URI: https://raftaar1191.com
+* Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
+* Author: Raftaar1191
+* Author URI: https://raftaar1191.com
+* Version: 1.0.0
+*/
+
